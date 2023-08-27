a5ca3943c635a2f52c368acdc5ef1bc8b702b371
William cardoso <williancard123@gmail.com>
2023-08-27 13:24:12 -0400

    fix bug database

--- mapa-didatico-backend/db/index.py
+++ mapa-didatico-backend/db/index.py
@@ -1,115 +1,73 @@
-import sqlite3, json
-from sqlite3 import Error
+import json
+import mysql.connector
 
+# Carregar dados do arquivo JSON
+with open('../process_data/data.json', 'r') as json_file:
+    data = json.load(json_file)
 
-def create_database(db_file):
+# Configurações do banco de dados
+db_config = {
+    'user': 'root',
+    'password': 'Bodepreto20!',
+    'host': 'localhost',
+    'database': '2022_metrics_database'
+}
+
+# Função para inserir dados na tabela "estacao"
+def insert_estacao(cursor, estacao_data):
+    insert_estacao_query = """ INSERT INTO estacao (regiao, uf, estacao, codigo_wmo, latitude, longitude, altitude)
+                               VALUES (%s, %s, %s, %s, %s, %s, %s); """
+    cursor.execute(insert_estacao_query, (estacao_data['REGIAO:'], estacao_data['UF:'], estacao_data['ESTACAO:'],
+                                          estacao_data['CODIGO (WMO):'], estacao_data['LATITUDE:'],
+                                          estacao_data['LONGITUDE:'], estacao_data['ALTITUDE:']))
+
+# Função para inserir dados na tabela "metricas"
+def insert_metricas(cursor, id_estacao, metricas_data):
+    metricas_data['data'] = metricas_data['data'].replace('/', '-')
+    insert_metricas_query = """ INSERT INTO metricas (id_estacao, data, hora, temp_maxi_h)
+                               VALUES (%s, %s, %s, %s); """
+    # temp is float or ''
+    temp = metricas_data['temp_maxi_h'] if metricas_data['temp_maxi_h'] != '' else None
+    cursor.execute(insert_metricas_query, (id_estacao, metricas_data['data'], metricas_data['hora'], temp))
+
+# Conectar ao banco de dados MySQL
+conn = mysql.connector.connect(**db_config)
+cursor = conn.cursor()
+
+# Definir queries para criação de tabelas
+sql_create_table_metricas = """ CREATE TABLE IF NOT EXISTS metricas (
+                                id int AUTO_INCREMENT PRIMARY KEY,
+                                id_estacao int NOT NULL,
+                                data date NOT NULL,
+                                hora time NOT NULL,
+                                temp_maxi_h float
+                            ); """
+
+sql_create_table_estacao = """ CREATE TABLE IF NOT EXISTS estacao (
+                                id int AUTO_INCREMENT PRIMARY KEY,
+                                regiao varchar(2) NOT NULL,
+                                uf varchar(2) NOT NULL,
+                                estacao varchar(50) NOT NULL,
+                                codigo_wmo varchar(10) NOT NULL,   
+                                latitude varchar(20) NOT NULL,
+                                longitude varchar(20) NOT NULL,
+                                altitude varchar(20) NOT NULL
+                            ); """
+
+# Executar queries de criação de tabelas
+cursor.execute(sql_create_table_metricas)
+cursor.execute(sql_create_table_estacao)
+conn.commit()
+
+# Processar e inserir dados do JSON
+for item in data:
+    insert_estacao(cursor, item['metadata'])
+    id_estacao = cursor.lastrowid
+    for metricas in item['data']:
+        insert_metricas(cursor, id_estacao, metricas)
+    conn.commit()
+
+# Fechar conexão com o banco de dados
+cursor.close()
+conn.close()
    
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
-    except Error as e:
-        print(e)
-    finally:
-        if conn:
-            conn.close()
-
-
-
-
-def main():
-
-    # create table for informations:
-    """
-     {
-        "metadata": {
-            "REGIAO:": "CO",
-            "UF:": "DF",
-            "ESTACAO:": "BRASILIA",
-            "CODIGO (WMO):": "A001",
-            "LATITUDE:": "-15,78944444",
-            "LONGITUDE:": "-47,92583332",
-            "ALTITUDE:": "1160,96"
-        },
-        "data": [
-            {
-                "data": "2022/01/01",
-                "hora": "09:00",
-                "temp_maxi_h": "17.5"
-            },]
-    """
-
-    sql_create_table_metricas = """ CREATE TABLE IF NOT EXISTS metricas (
-                                        id integer PRIMARY KEY,
-                                        id_estacao integer NOT NULL,
-                                        data text NOT NULL,
-                                        hora text NOT NULL,
-                                        temp_maxi_h text NOT NULL
-                                    ); """
-
-    sql_create_table_estacao = """ CREATE TABLE IF NOT EXISTS estacao (
-                                        id integer PRIMARY KEY,
-                                        regiao text NOT NULL,
-                                        uf text NOT NULL,
-                                        estacao text NOT NULL,
-                                        codigo_wmo text NOT NULL,   
-                                        latitude text NOT NULL,
-                                        longitude text NOT NULL,
-                                        altitude text NOT NULL
-                                    ); """
-    
-
-    try:
-        conn = sqlite3.connect(r"2022_metrics_climate.db")
-        
-        # create tables
-        if conn is not None:
-            # create metricas table
-            cur = conn.cursor()
-            cur.execute(sql_create_table_metricas)
-            # create estacao table
-            cur = conn.cursor()
-            cur.execute(sql_create_table_estacao)
-        else:
-            print("Error! cannot create the database connection.")
-    except Error as e:
-        print(e)
-    finally:
-        if conn:
-            conn.close()
-
-    try:
-        conn = sqlite3.connect(r"2022_metrics_climate.db")
-        
-        # create tables
-        if conn is not None:
-            # insert data in tables
-            with open("../process_data/data.json", "r") as f:
-                data = json.load(f)
-                for i in data:
-                    # insert data in estacao table
-                    sql = ''' INSERT INTO estacao(regiao,uf,estacao,codigo_wmo,latitude,longitude,altitude)
-                              VALUES(?,?,?,?,?,?,?) '''
-                    cur = conn.cursor()
-                    cur.execute(sql, (i["metadata"]["REGIAO:"],i["metadata"]["UF:"],i["metadata"]["ESTACAO:"],i["metadata"]["CODIGO (WMO):"],i["metadata"]["LATITUDE:"],i["metadata"]["LONGITUDE:"],i["metadata"]["ALTITUDE:"]))
-                    conn.commit()
-                    # insert data in metricas table
-                    for j in i["data"]:
-                        sql = ''' INSERT INTO metricas(id_estacao,data,hora,temp_maxi_h)
-                                  VALUES(?,?,?,?) '''
-                        cur = conn.cursor()
-                        cur.execute(sql, (i["metadata"]["CODIGO (WMO):"], j["data"], j["hora"], j["temp_maxi_h"]))
-                        conn.commit()
-        else:
-            print("Error! cannot create the database connection.")
-    except Error as e:
-        print(e)
-    finally:
-        if conn:
-            conn.close()
-
-    
-
-if __name__ == '__main__':
-    create_database(r"2022_metrics_climate.db")
-    main()
