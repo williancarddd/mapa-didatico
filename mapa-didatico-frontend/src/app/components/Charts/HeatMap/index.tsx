c77a7ab50aa5def39fdd87e6f0fc6eb900f61bcc
William Cardoso <williancard123@gmail.com>
2023-09-19 18:56:38 -0300

    regressão linear implementada e dados de qualquer ano agora é possivel usar.

---
+++ mapa-didatico-frontend/src/app/components/Charts/HeatMap/index.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import ReactApexChart from 'react-apexcharts';
+
+interface APIAverageData {
+  dia: number;
+  mes: number;
+  ano: number;
+  media_temp: number;
+}
+
+interface HeatmapChartProps {
+  data: APIAverageData[];
+  title: string;
+}
+
+const HeatmapChart: React.FC<HeatmapChartProps> = ({ data, title }) => {
+  // Organize os dados em um formato adequado para o gráfico de calor
+  const monthNames = [
+    'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+    'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+  ];
+
+  const maxDaysInMonth = Math.max(
+    ...monthNames.map((month, index) =>
+      data.filter(item => item.mes === index + 1).length
+    )
+  );
+
+  const daysArray = Array.from({ length: maxDaysInMonth }, (_, index) => index + 1);
+
+  const heatmapData = monthNames.map((month, index) => {
+    const monthData = data.filter(item => item.mes === index + 1);
+    const seriesData = daysArray.map(day => {
+      const dayData = monthData.find(item => item.dia === day);
+      return dayData ? dayData.media_temp : null;
+    });
+
+    return {
+      name: month,
+      data: seriesData,
+    };
+  });
+
+  return (
+    <div>
+      <ReactApexChart
+        options={{
+          chart: {
+            type: 'heatmap',
+            toolbar: {
+              show: false,
+            },
+          },
+          plotOptions: {
+            heatmap: {
+              shadeIntensity: 0.5, // Reduza a intensidade da sombra
+              radius: 0, // Remova o raio
+              useFillColorAsStroke: true,
+              enableShades: true,
+              distributed: true,
+              reverseNegativeShade: true,
+            },
+          },
+          xaxis: {
+            type: 'category',
+            categories: daysArray,
+            title: {
+              text: 'Dia',
+            },
+          },
+          yaxis: {
+            title: {
+              text: 'Mês',
+            },
+          },
+          dataLabels: {
+            enabled: false,
+          },
+          colors: ['#00A100', '#2EB82E', '#128FD9', '#FFB200', '#FF0000'],
+          title: {
+            text: title,
+          },
+        }}
+        series={heatmapData}
+        type="heatmap"
+        height={350}
+      />
+    </div>
+  );
+};
+
+export default HeatmapChart;
